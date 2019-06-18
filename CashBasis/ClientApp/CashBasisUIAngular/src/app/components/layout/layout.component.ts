b3c37ba07abb2912424d82a27cc2c05921a5f330
Mile Mircheski <mirceski91@gmail.com>
2019-06-18 21:56:35 +0200

    added layout component

---
+++ CashBasis/ClientApp/CashBasisUIAngular/src/app/components/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: []
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
